d103dc812355792e64f2f76beb53b1401e121423
Saif <saif@visionituk.com>
2021-10-11 00:15:15 +0100

    (server-side) implement rooms

--- server/src/chat.js
+++ server/src/chat.js
@@ -3,6 +3,14 @@ import { EventEmitter } from "events"
 
 const users = new Map()
 const emitter = new EventEmitter()
+const rooms = {
+	general: {
+		messages: []
+	},
+	"not-general": {
+		messages: []
+	}
+}
 
 const init = (socket) => {
 	socket.on(`login-request`, async data => {
@@ -12,12 +20,13 @@ const init = (socket) => {
 			return socket.emit(`incorrect-password`)
 		}
 
-		users.set(socket, username)
+		const room = rooms[0]
+		users.set(socket, { username, socket, room })
 
 		socket.emit(`welcome`, {
 			username,
 			users: Object.values(users),
-			time: ``
+			rooms
 		})
 
 		emitter.emit(`user-join`, username)
@@ -26,9 +35,10 @@ const init = (socket) => {
 	socket.on(`chat-message`, content => {
 		if (!content || !content.length) return
 
-		const sender = users.get(socket)
-		if (sender) {
-			const message = { content, sender }
+		const user = users.get(socket)
+		if (user) {
+			const { username: sender, room } = user
+			const message = { content, sender, room }
 			emitter.emit(`chat-message`, message)
 		}
 	})
@@ -37,16 +47,27 @@ const init = (socket) => {
 		const user = users.get(socket)
 
 		if (user) {
-			emitter.emit(`user-leave`, user)
+			const { username } = user
+			emitter.emit(`user-leave`, username)
 			users.delete(socket)
 		}
 	})
 }
 
-const broadcast = (key, data) => Array.from(users.keys()).forEach(socket => socket.emit(key, data))
+const broadcast = (key, data, room = undefined) => {
+	console.log(`sending ${key} to ${room}`)
 
-emitter.on(`chat-message`, data => broadcast(`chat-message`, data))
+	let receivers = Array.from(users.keys())
+	if (room) {
+		receivers = receivers.filter(key => users.get(key).room == room)
+	}
+	receivers.forEach(socket => socket.emit(key, data))
+}
+
+emitter.on(`chat-message`, data => broadcast(`chat-message`, data, data.room))
+emitter.on(`chat-message`, data => rooms[data.room].push({ content: data.content, sender: data.sender }))
 emitter.on(`user-join`, user => broadcast(`user-join`, user))
 emitter.on(`user-leave`, user => broadcast(`user-leave`, user))
+
 
 export default { ...emitter, init }
